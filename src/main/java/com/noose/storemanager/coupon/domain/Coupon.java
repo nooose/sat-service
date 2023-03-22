c798a76932e96b8b9ea7d55300e44b2ae1bedd67
nonoro <kgu1022@naver.com>
2023-03-22 18:55:10 +0900

    쿠폰 테스트
    - Coupon
     - 쿠폰 이름, 설명 길이 검증 로직 추가
     - CouponResponse 로 형변환 해주는 로직 추가
    
    - CouponTest
     - 쿠폰의 이름과 쿠폰 설명의 길이에 대한 테스트

--- src/main/java/com/noose/storemanager/coupon/domain/Coupon.java
+++ src/main/java/com/noose/storemanager/coupon/domain/Coupon.java
@@ -1,5 +1,7 @@
 package com.noose.storemanager.coupon.domain;
 
+import com.noose.storemanager.coupon.application.dto.request.CouponRequest;
+import com.noose.storemanager.coupon.application.dto.response.CouponResponse;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,7 +21,15 @@ public class Coupon {
     private String description;
 
     public Coupon(String name, String description) {
+        if (name.length() < 2 || name.length() > 10 || description.length() > 30) {
+            throw new IllegalArgumentException();
+        }
+
         this.name = name;
         this.description = description;
+    }
+
+    public CouponResponse toResponse() {
+        return new CouponResponse(id, name, description);
     }
 }
--- src/test/java/com/noose/storemanager/coupon/domain/CouponTest.java
+++ src/test/java/com/noose/storemanager/coupon/domain/CouponTest.java
@@ -11,14 +11,14 @@ class CouponTest {
     @DisplayName("쿠폰의 이름은 2 글자 미만이거나 10 글자를 초과하는 경우 예외가 발생한다.")
     @Test
     void couponNameException() {
-        assertThatThrownBy(() -> new Coupon("", ""))
+        assertThatThrownBy(() -> new Coupon("2", ""))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("쿠폰 설명이 30 글자를 초과하는 경우 예외가 발생한다.")
     @Test
     void couponDescriptionException() {
-        assertThatThrownBy(() -> new Coupon("쿠폰", ""))
+        assertThatThrownBy(() -> new Coupon("쿠폰", "가나다라마바사아자차카타파하아야어여오요우유으이30글자로올라가라올라가쭉쭉쭉올라가랏!"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
