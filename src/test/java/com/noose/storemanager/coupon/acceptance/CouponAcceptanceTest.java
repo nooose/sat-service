02a90eadbf56851ad8986bacac75966735ce9a7a
nonoro <kgu1022@naver.com>
2023-03-25 15:06:51 +0900

    테스트 시나리오 개편
    추가로 spring이 지원해주는 MediaType 상수 사용
    불필요한 `@ParameterizedTest` 제거

--- src/test/java/com/noose/storemanager/coupon/acceptance/CouponAcceptanceTest.java
+++ src/test/java/com/noose/storemanager/coupon/acceptance/CouponAcceptanceTest.java
@@ -2,13 +2,11 @@ package com.noose.storemanager.coupon.acceptance;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import org.apache.http.HttpHeaders;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -34,16 +32,19 @@ public class CouponAcceptanceTest extends AcceptanceTest {
         params.put("name", "쿠폰");
         params.put("description", "테스트");
         ExtractableResponse<Response> response = given().log().all()
-                .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(params)
                 .when().post("/coupons")
                 .then().log().all()
                 .extract();
 
+        String[] location = response.headers().get("Location").getValue().split("/");
+        int createdCouponId = Integer.parseInt(location[location.length - 1]);
+        ExtractableResponse<Response> couponResponse = findCoupon(createdCouponId);
+
         assertAll(() -> {
             assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-            assertThat(response.jsonPath().getString("name")).isEqualTo("쿠폰");
-            assertThat(response.jsonPath().getString("description")).isEqualTo("테스트");
+            assertThat(couponResponse.jsonPath().getInt("id")).isEqualTo(createdCouponId);
         });
     }
 
@@ -52,15 +53,14 @@ public class CouponAcceptanceTest extends AcceptanceTest {
             Map<String, String> params = new HashMap<>();
             params.put("name", "쿠폰");
             params.put("description", "테스트");
-            ExtractableResponse<Response> response = given().log().all()
-                    .header(HttpHeaders.CONTENT_TYPE, "application/json")
+            given().log().all()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
                     .body(params)
                     .when().post("/coupons")
                     .then().log().all()
                     .extract();
         }
     }
-
 
     /**
      * When 쿠폰을 조회하면
@@ -71,56 +71,53 @@ public class CouponAcceptanceTest extends AcceptanceTest {
         int createCouponNumber = 4;
         createCoupon(createCouponNumber);
 
-        ExtractableResponse<Response> response = findAllCoupon();
-
-        assertAll(() -> {
-            assertThat(response.jsonPath().getList("name").size()).isEqualTo(createCouponNumber);
-            assertThat(response.jsonPath().getList("description").size()).isEqualTo(createCouponNumber);
-        });
-    }
-
-    private ExtractableResponse<Response> findAllCoupon() {
-        return given().log().all()
+        ExtractableResponse<Response> response = given().log().all()
                 .when().get("/coupons")
                 .then().log().all()
                 .extract();
-    }
 
+        assertThat(response.jsonPath().getList("id").size()).isEqualTo(createCouponNumber);
+    }
 
     /*
      * When 쿠폰번호를 조회하면
      * Then 쿠폰번호에 해당하는 쿠폰을 확인할 수 있다.
      * */
-    @ParameterizedTest(name = "{0} = {1}")
-    @CsvSource({"name,쿠폰", "description,테스트"})
-    void showCouponOfOne(String path, String expected) {
+    @Test
+    void showCouponOfOne() {
         int createCouponNumber = 6;
         createCoupon(createCouponNumber);
 
-        ExtractableResponse<Response> response = given().log().all()
-                .when().get("/coupons/2")
+        int findCouponId = 2;
+        ExtractableResponse<Response> response = findCoupon(findCouponId);
+
+        assertThat(response.jsonPath().getInt("id")).isEqualTo(2);
+    }
+
+    private ExtractableResponse<Response> findCoupon(int id) {
+        return given().log().all()
+                .when().get("/coupons/" + id)
                 .then().log().all()
                 .extract();
-
-        assertThat(response.jsonPath().getString(path)).isEqualTo(expected);
     }
 
     /*
      * When 쿠폰번호 삭제요청을 하면
      * Then 쿠폰이 삭제된다.
      * */
-    @ParameterizedTest
-    @CsvSource({"name,10", "description,10"})
-    void deleteCoupon(String path, int numberOfCoupon) {
-        createCoupon(numberOfCoupon);
+    @Test
+    void deleteCoupon() {
+        int createCouponNumber = 6;
+        int deleteCouponNumber = 3;
+        createCoupon(createCouponNumber);
 
         given().log().all()
-                .when().delete("/coupons/3")
+                .when().delete("/coupons/" + deleteCouponNumber)
                 .then().log().all()
                 .extract();
 
-        ExtractableResponse<Response> coupons = findAllCoupon();
+        ExtractableResponse<Response> couponResponse = findCoupon(deleteCouponNumber);
 
-        assertThat(coupons.jsonPath().getList(path).size()).isEqualTo(--numberOfCoupon);
+        assertThat(couponResponse.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 }
