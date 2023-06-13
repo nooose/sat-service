4f17ead67503c1cace3e83d2de401815fa24873d
nooose <hd15807@gmail.com>
2023-06-13 13:33:35 +0900

    #3 - 이전 소스를 모두 제거함

--- src/main/java/com/noose/storemanager/coupon/application/dto/request/CouponRequest.java
+++
@@ -1,7 +0,0 @@
-package com.noose.storemanager.coupon.application.dto.request;
-
-public record CouponRequest(
-    String name,
-    String description
-) {
-}
--- src/main/java/com/noose/storemanager/coupon/repository/CouponRepository.java
+++
@@ -1,7 +0,0 @@
-package com.noose.storemanager.coupon.repository;
-
-import com.noose.storemanager.coupon.domain.Coupon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CouponRepository extends JpaRepository<Coupon, Long> {
-}
