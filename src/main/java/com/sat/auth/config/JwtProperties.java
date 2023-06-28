9813ccc030045d91847ce7cf63e39c443b75660b
nonoro <kgu1022@naver.com>
2023-06-28 10:41:01 +0900

    #8 - jwt 토큰생성시 필요한 property를 받을 record와 property 정보가 들어있는 yaml 파일 작성

--- src/main/java/com/sat/auth/config/JwtProperties.java
+++ src/main/java/com/sat/auth/config/JwtProperties.java
@@ -1,26 +1,16 @@
 package com.sat.auth.config;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.convert.DurationUnit;
 
 import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 
 @ConfigurationProperties("jwt")
 public record JwtProperties (
         String secretKey,
-        Access access,
-        Refresh refresh
+        TokenConfig access,
+        TokenConfig refresh
 ) {
-    public record Access(
-            Duration expirationTime,
-            String header) {
-    }
-
-    public record Refresh(
-            Duration expirationTime,
-            String header) {
+    public record TokenConfig(
+            Duration expirationTime) {
     }
 }
