45f645eacd38bb5dfd5927e6649415872cd7e861
nooose <hd15807@gmail.com>
2023-06-29 11:42:15 +0900

    #8 - 불필요한 토큰 테스트 케이스 제거
    만료시간 검증만 하면 될 것 같다

--- src/test/groovy/com/sat/auth/application/JwtServiceTest.groovy
+++ src/test/groovy/com/sat/auth/application/JwtServiceTest.groovy
@@ -17,99 +17,19 @@ class JwtServiceTest extends Specification {
     private JwtService jwtService
 
     @Autowired
-    private JwtProperties jwtProperties
+    private JwtProperties properties
 
-    def "Jwt 엑세스 토큰을 만든다."() {
-        expect:
-        def accessToken = jwtService.createAccessToken(12321321L)
-        accessToken != null
-    }
-
-    def "Jwt 리프레시 토큰을 만든다"() {
-        expect:
-        def refreshToken = jwtService.createRefreshToken()
-        refreshToken != null
-    }
-
-    def "access 토큰의 유효기간 만료 유무를 검사한다."() {
+    def "기간이 지난 토큰은 유효하지 않다고 판단한다"() {
         given:
-        def accessToken = jwtService.createAccessToken(12321321L)
-        Thread.sleep(3000)
-
-        when:
-        jwtService.validExpiredTime(accessToken)
-
-        then:
-        thrown(TokenExpiredException.class)
-    }
-
-    void "access 토큰의 유효성을 검사한다.(유효하지 않은 경우)"() {
-        given:
-        Date now = new Date();
         String token = JWT.create()
-                .withSubject("AccessToken")
-                .withExpiresAt(new Date(now.getTime() + jwtProperties.access().expirationTime().toMillis()))
-                .withClaim("socialId", 24332425L)
-                .sign(Algorithm.HMAC512("24324wefffwfwffjafkjLKJ@#@*@#*%&*#U@JfSDHFh") )
+                .withSubject("test")
+                .withExpiresAt(new Date())
+                .sign(Algorithm.HMAC512(properties.secretKey()) )
 
         when:
         def valid = jwtService.isValidToken(token)
 
         then:
-        if (valid) {
-            println "유효한 토큰입니다"
-        } else {
-            println "유효하지 않은 토큰입니다."
-        }
-
+        !valid
     }
-
-    void "access 토큰의 유효성을 검사한다.(유효한 경우)"() {
-        given:
-        def token = jwtService.createAccessToken(12321421412L)
-
-        when:
-        def valid = jwtService.isValidToken(token)
-
-        then:
-        if (valid) {
-            println "유효한 토큰입니다"
-        } else {
-            println "유효하지 않은 토큰입니다."
-        }
-
-    }
-
-    def "refresh 토큰의 유효기간 만료 유무를 검사한다."() {
-        given:
-        def refreshToken = jwtService.createRefreshToken()
-        Thread.sleep(7000)
-
-        when:
-        jwtService.validExpiredTime(refreshToken)
-
-        then:
-        thrown(TokenExpiredException.class)
-    }
-
-    void "refresh 토큰의 유효성을 검사한다.(유효하지 않은 경우)"() {
-        given:
-        Date now = new Date();
-        String token = JWT.create()
-                .withSubject("RefreshToken")
-                .withExpiresAt(new Date(now.getTime() + jwtProperties.refresh().expirationTime().toMillis()))
-                .sign(Algorithm.HMAC512("24324wefffwfwffjafkjLKJ@#@*@#*%&*#U@JfSDHFh") )
-
-        when:
-        def valid = jwtService.isValidToken(token)
-
-        then:
-        if (valid) {
-            println "유효한 토큰입니다"
-        } else {
-            println "유효하지 않은 토큰입니다."
-        }
-
-    }
-
 }
