a41c6fc1197fd33d3b48720f1f3afeef55d8887a
nooose <hd15807@gmail.com>
2023-07-04 16:21:19 +0900

    #12 - oauth 응답 객체 매핑 수정 및 h2 설정
    - 필드명이 잘못입력되어 객체 바인딩이 되지 않았음
    - 개발편의를 위해 H2 console 접근 허용되도록 변경함

--- src/main/java/com/sat/auth/application/dto/KakaoOAuth2Response.java
+++ src/main/java/com/sat/auth/application/dto/KakaoOAuth2Response.java
@@ -5,22 +5,29 @@ import java.util.Map;
 public record KakaoOAuth2Response(
         String id,
         Map<String, Object> properties,
-        Profile profile
+        KakaoAccount kakaoAccount
 ) {
-    public record Profile(
-            String nickname
+    public record KakaoAccount(
+            Profile profile
     ) {
+        public record Profile(
+                String nickname
+        ) {
+            public static Profile of(Map<String, Object> attributes) {
+                return new Profile(String.valueOf(attributes.get("nickname")));
+            }
+        }
 
-        public static Profile of(Map<String, Object> attributes) {
-            return new Profile(String.valueOf(attributes.get("nickname")));
+        public static KakaoAccount of(Map<String, Object> attributes) {
+            return new KakaoAccount(Profile.of((Map<String, Object>) attributes.get("profile")));
         }
     }
-
 
     public static KakaoOAuth2Response of(Map<String, Object> attributes) {
         return new KakaoOAuth2Response(
                 String.valueOf(attributes.get("id")),
                 (Map<String, Object>) attributes.get("properties"),
-                Profile.of((Map<String, Object>) attributes.get("profile")));
+                KakaoAccount.of((Map<String, Object>) attributes.get("kakao_account"))
+        );
     }
 }
