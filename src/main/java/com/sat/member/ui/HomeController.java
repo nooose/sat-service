3af3389d46d13974fb2d28f753c23348dd4898ea
nooose <hd15807@gmail.com>
2023-07-17 18:22:38 +0900

    #11 - 인증 테스트 용 API 추가
    인증된 id를 확인할 수 있게 만들었다.
    시간이 지나면 삭제될 예정이다

--- src/main/java/com/sat/member/ui/HomeController.java
+++ src/main/java/com/sat/member/ui/HomeController.java
@@ -1,6 +1,7 @@
 package com.sat.member.ui;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,11 @@ public class HomeController {
     @GetMapping("/")
     public ResponseEntity<Void> home() {
         return ResponseEntity.ok().build();
+    }
+
+    @Deprecated
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello(@AuthenticationPrincipal String principal) {
+        return ResponseEntity.ok().body("hello " + principal);
     }
 }
