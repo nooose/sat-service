330af7b5745e5fad2e7f9b07034d1261d6458a80
nooose <hd15807@gmail.com>
2023-08-20 02:44:41 +0900

    #23 - 예외 응답 처리
    예외 발생 시 적절한 응답 메시지를 반환할 수 있게 규격 생성함

---
+++ src/main/java/com/sat/common/exception/CommonRestControllerAdvice.java
@@ -0,0 +1,39 @@
+package com.sat.common.exception;
+
+import com.sat.common.dto.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.format.DateTimeParseException;
+
+@Slf4j
+@RestControllerAdvice
+public class CommonRestControllerAdvice {
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<ErrorResponse> error(BindException e) {
+        ErrorResponse errorResponse = ErrorResponse.of(e);
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(DateTimeParseException.class)
+    public ResponseEntity<ErrorResponse> error(DateTimeParseException e) {
+        ErrorResponse errorResponse = ErrorResponse.of(e);
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(DataNotFoundException.class)
+    public ResponseEntity<ErrorResponse> error(DataNotFoundException e) {
+        ErrorResponse errorResponse = ErrorResponse.of(e);
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+//    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> error(IllegalArgumentException e) {
+        ErrorResponse errorResponse = ErrorResponse.of(e);
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+}
