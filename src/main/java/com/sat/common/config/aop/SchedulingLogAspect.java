57615bbc8039cb53bc31a940dc2a026f96e54587
nooose <hd15807@gmail.com>
2023-10-01 20:07:06 +0900

    #55 - around로 변경

--- src/main/java/com/sat/common/config/aop/SchedulingLogAspect.java
+++ src/main/java/com/sat/common/config/aop/SchedulingLogAspect.java
@@ -1,28 +1,24 @@
 package com.sat.common.config.aop;
 
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 
 @Slf4j
 @Aspect
 public class SchedulingLogAspect {
 
-    @Before(value = "@annotation(org.springframework.scheduling.annotation.Scheduled)")
-    public void doBefore(JoinPoint joinPoint) {
+    @Around("@annotation(org.springframework.scheduling.annotation.Scheduled)")
+    public Object around(ProceedingJoinPoint joinPoint) {
+        Object result = null;
         log.info("{} 스케줄러 시작", joinPoint.getSignature().toShortString());
-    }
-
-    @After(value = "@annotation(org.springframework.scheduling.annotation.Scheduled)")
-    public void doAfter(JoinPoint joinPoint) {
+        try {
+            result = joinPoint.proceed();
+        } catch (Throwable e) {
+            log.error("{} 스케줄러 예외", joinPoint.getSignature().toShortString(), e);
+        }
         log.info("{} 스케줄러 종료", joinPoint.getSignature().toShortString());
-    }
-
-    @AfterThrowing(value = "@annotation(org.springframework.scheduling.annotation.Scheduled)", throwing = "e")
-    public void doThrowing(JoinPoint joinPoint, Exception e) {
-        log.error("{} 스케줄러 예외", joinPoint.getSignature().toShortString(), e);
+        return result;
     }
 }
