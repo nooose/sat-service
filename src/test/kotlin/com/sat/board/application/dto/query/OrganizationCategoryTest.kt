69bfa28aa0ce19687be0b83854c3a156cf68a5d9
nonoro <kgu1022@naver.com>
2024-03-09 21:50:01 +0900

    test: 카테고리 수정/삭제 기능이 추가에 따른 테스트 코드 작성
    깨지는 코드에 대응하여 수정한다

--- src/test/kotlin/com/sat/board/application/dto/query/OrganizationCategoryTest.kt
+++ src/test/kotlin/com/sat/board/application/dto/query/OrganizationCategoryTest.kt
@@ -1,6 +1,7 @@
 package com.sat.board.application.dto.query
 
 import com.sat.board.domain.Category
+import com.sat.board.domain.CategoryName
 import io.kotest.core.spec.DisplayName
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
@@ -9,12 +10,12 @@ import io.kotest.matchers.shouldBe
 class OrganizationCategoryTest : BehaviorSpec({
     Given("부모 카테고리와 자식카테고리가 섞인 카테고리 리스트가 있고") {
         val categories = listOf(
-            Category("컴퓨터", null, 1L),
-            Category("애플", 1L, 2L),
-            Category("삼성", 1L, 3L),
-            Category("맥북프로", 2L, 5L),
-            Category("맥북에어", 2L, 6L),
-            Category("갤럭시북", 3L, 7L),
+            Category(CategoryName("컴퓨터"), null, 1L),
+            Category(CategoryName("애플"), 1L, 2L),
+            Category(CategoryName("삼성"), 1L, 3L),
+            Category(CategoryName("맥북프로"), 2L, 5L),
+            Category(CategoryName("맥북에어"), 2L, 6L),
+            Category(CategoryName("갤럭시북"), 3L, 7L),
         )
         When("계층을 만들면") {
             val organizationCategories = OrganizationCategory(categories)
