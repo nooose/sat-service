5099db998585e4368fcf8749f38181606d7678f2
nooose <hd15807@gmail.com>
2024-04-20 20:10:51 +0900

    #76 - feat: 깜빡거림 해결
    야매다

--- frontend/app/authProvider.tsx
+++ frontend/app/authProvider.tsx
@@ -1,17 +1,17 @@
-"use client"
-
-import React, {useEffect} from "react";
+import React, {useEffect, useLayoutEffect, useState} from "react";
 import authStore from "@/store/auth-store";
 import {get} from "@/utils/client";
 
 export function LoginProvider({children}: { children: React.ReactNode }) {
     const store = authStore((state: any) => state);
+    const [ok, setOk] = useState(false);
+
     const fetchData = async () => {
         if (store.authenticated) {
             return;
         }
-
-        const response = await get("/user/members/me");
+        const response = await get("/user/members/me")
+            .finally(() => setOk(true));
         if (response.ok) {
             const json = await response.json();
             store.setName(json.name);
@@ -25,7 +25,7 @@ export function LoginProvider({children}: { children: React.ReactNode }) {
 
     return (
         <React.Fragment>
-            {children}
+            {ok && children}
         </React.Fragment>
     )
 }
