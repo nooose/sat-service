8dc1de453fb65449d40b194520fc48a41ad3152e
nooose <hd15807@gmail.com>
2024-04-27 19:25:03 +0900

    #76 - fix: Events lateinit 수정

--- src/main/kotlin/com/sat/common/utils/event/Events.kt
+++ src/main/kotlin/com/sat/common/utils/event/Events.kt
@@ -3,17 +3,17 @@ package com.sat.common.utils.event
 import org.springframework.context.ApplicationEventPublisher
 
 object Events {
-    private lateinit var publisher: ApplicationEventPublisher
+    private var publisher: ApplicationEventPublisher? = null
 
     fun initialize(publisher: ApplicationEventPublisher) {
-        if (publisher == null) {
+        if (this.publisher == null) {
             this.publisher = publisher
         }
     }
 
     fun publish(event: Any) {
         if (publisher != null) {
-            publisher.publishEvent(event)
+            publisher!!.publishEvent(event)
         }
     }
 }
