c18feb363149bfaafe2efb7bd6206d2bb7306507
nooose <hd15807@gmail.com>
2024-08-03 17:42:04 +0900

    refactor: redis rtt 줄이기
    통신 비용을 아낄 수 있다.

--- src/main/kotlin/com/sat/user/command/application/PointCacheInitializer.kt
+++ src/main/kotlin/com/sat/user/command/application/PointCacheInitializer.kt
@@ -3,19 +3,22 @@ package com.sat.user.command.application
 import com.sat.common.domain.RedisCacheName
 import com.sat.user.query.PointQueryService
 import org.springframework.beans.factory.InitializingBean
+import org.springframework.data.redis.core.DefaultTypedTuple
 import org.springframework.data.redis.core.RedisTemplate
-import org.springframework.stereotype.Service
+import org.springframework.stereotype.Component
 
-@Service
-class PointCacheService(
+@Component
+class PointCacheInitializer(
     private val pointQueryService: PointQueryService,
     private val redisTemplate: RedisTemplate<String, Any>,
 ): InitializingBean {
     override fun afterPropertiesSet() {
         val totalPointsOfMembers = pointQueryService.getTotalPointsOfMembers()
         val pointRankingZSetOps = redisTemplate.opsForZSet()
-        totalPointsOfMembers.forEach {
-            pointRankingZSetOps.add(RedisCacheName.RANKING.key, it.memberId, it.point.toDouble())
-        }
+
+        val tupleSet = totalPointsOfMembers.map {
+            DefaultTypedTuple(it.memberId as Any, it.point.toDouble())
+        }.toSet()
+        pointRankingZSetOps.add(RedisCacheName.RANKING.key, tupleSet)
     }
 }
